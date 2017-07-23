2e9f2569a60f8da1cd15a54b09809c78d749202b
anmakhort <anmakhort@gmail.com>
2017-07-24 00:57:41 +0200

    Function was moved from the main.c

---
+++ src/createimg.c
@@ -0,0 +1,9 @@
+#include "../include/connectfour.h"
+
+XImage *create_frame_image(Display *disp, Visual *vis, int depth, char **buff) {
+	if (!disp || !vis || depth < 24 || !buff) return NULL;
+	if (*buff) free(*buff);
+	*buff = malloc(4 * sizeof(char) * BF_WIDTH * BF_HEIGHT);
+	return XCreateImage(disp, vis, depth, ZPixmap, 0, *buff, \
+		BF_WIDTH, BF_HEIGHT, 32, 0);
+}
